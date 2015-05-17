b54bf2efd405d7e5765b85e9e9a20d49e8b5e71f
Andrew James <andrewpjames87@gmail.com>
2015-05-17 11:10:16 -0600

    Wrote function for cross browser home page scroll but it does not work

--- app/assets/javascripts/homescrolling.js
+++ app/assets/javascripts/homescrolling.js
@@ -6,16 +6,16 @@ var homeScroll = (function(){
       e.preventDefault();
       $('.welcome-page').animate({scrollTop: $(scrollTo).offset().top, easing: 'easeInOutQuart', duration: 'slow'}, 1000);
     });
-
-  // function scrollToAnchor(anchor){
-  // $('.welcome-page').animate({scrollTop: $(anchor).offset().top, easing: 'easeInOutQuart', duration: 'slow'}, 1000);
-  // }
-  //
-  // $('.welcome-page .header a, #sidebar a').on('click', function() {
-  //   var scrollTo = $.attr(this, 'href').replace('/', '');
-  //   scrollToAnchor(scrollTo);
-  // });
   };
+
+  // var scrollToAnchor = function(){
+  //   $('.welcome-page .header a, #sidebar a').on('click', function() {
+  //     var scrollTo = $.attr(this, 'href').replace('/', '');
+  //     console.log(scrollTo);
+  //   $('.welcome-page').animate({scrollTop: $(scrollTo).offset().top, easing: 'easeInOutQuart'}, 'slow');
+  //   });
+  // };
+
 
   var init = function(){
     scrollToAnchor();
