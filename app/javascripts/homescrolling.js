767ae04a6b5adec1f15f1d8f60c7e12d6d28fa64
Andrew James <andrewpjames87@gmail.com>
2015-05-18 22:26:49 -0600

    Fixed FireFox jquery issues

--- app/javascripts/homescrolling.js
+++ app/javascripts/homescrolling.js
@@ -2,15 +2,42 @@ $(document).ready(function() {
 
   var homeScroll = (function(){
 
-    var scrollToAnchor = function(){
-      $('.welcome-page .header a, #sidebar a').on('click', function(e) {
-        var scrollTo = $.attr(this, 'href').replace('/', '');
-        e.preventDefault();
-        $('.welcome-page').animate({scrollTop: $(scrollTo).offset().top, easing: 'easeInOutQuart', duration: 'slow'}, 1000);
-        // $('.welcome-page').animate({scrollTop:parseInt($(scrollTo).css('top'))}, 1000, 'easeInOutExpo');
-        // return false;
-      });
-    };
+    // var scrollToAnchor = function(){
+    //   $('.welcome-page .header a, #sidebar a').on('click', function(e) {
+    //     var scrollTo = $.attr(this, 'href').replace('/', '');
+    //     e.preventDefault();
+    //     $('.welcome-page').animate({scrollTop: $(scrollTo).offset().top, easing: 'easeInOutQuart', duration: 'slow'}, 1000);
+    //   });
+    // };
+
+    // $('a[href=#portfolio]').click(function(){
+  	// 		//$('#vertScroll').animate({scrollTop:$('#proj1').position().top}, 0);
+    //     var scrollTo = $.attr(this, 'href').replace('/', '');
+  	// 		$('.welcome-page').animate({scrollTop:$(scrollTo).css('top')}, 1000, 'easeInOutExpo');
+  	// 		return false;
+  	// 	});
+
+//   		$('.container').each(function(){
+//   			//Setting the top property for CSS to the position().top [jQuery]
+//   			//The top was set to auto - but to be overwritten by position().top
+//   			//position().top = 5100 set css('top') = 5100px
+//   			$(this).css({'top': $(this).position().top});
+//   		});
+//
+//   		$('a[href=#portfolio]').click(function(){
+//         console.log('hi');
+//   			var targetDiv = $(this).attr('href');
+//
+//   			var listItem = $(targetDiv);
+//   			var targetIndex = ($(".container > div").index(listItem));
+//   			var destination = ((parseInt($('.container').css('height'))+parseInt($('.container').css('padding-bottom')))*targetIndex);
+// console.log(destination);
+//         debugger;
+//   												//scrollTop uses a integer to scroll - parseInt to remove the 'px' from the top property of the CSS
+//   												// parse(5100px) = 5100
+//   			$('.welcome-page').animate({scrollTop:destination}, 1000, 'easeInOutExpo');
+//   			return false;
+//   		});
 
     // var scrollToAnchor = function(){
     //   $('.welcome-page .header a, #sidebar a').on('click', function() {
@@ -20,6 +47,17 @@ $(document).ready(function() {
     //   });
     // };
 
+    var scrollToAnchor = function(){
+
+    $('.header a, #sidebar a').click(function(e){
+      e.preventDefault();
+      console.log('hi');
+        $('html,body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 800);
+        return false;
+    });
+    };
 
     var init = function(){
       scrollToAnchor();
